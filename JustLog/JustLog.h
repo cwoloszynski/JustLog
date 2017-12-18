7355c0c636927644250e9801c2b0c3aa35919cd0
Charlie Woloszynski <charles.woloszynski@handheldmedia.us>
2017-12-18 09:46:29 -0500

    Add xcodeproj and fix Logger to reference UIKit

---
+++ JustLog/JustLog.h
@@ -0,0 +1,19 @@
+//
+//  HHMJustLog.h
+//  HHMJustLog
+//
+//  Created by Charlie Woloszynski on 12/18/17.
+//  Copyright © 2017 Handheld Media, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HHMJustLog.
+FOUNDATION_EXPORT double JustLogVersionNumber;
+
+//! Project version string for HHMJustLog.
+FOUNDATION_EXPORT const unsigned char JustLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHMJustLog/PublicHeader.h>
+
+
